d44ec8e94138f64305a47a4b8e9003aad172f894
goshag45 <goshagugl44@gmail.com>
2023-03-26 17:39:11 +1000

    update jobcollection tests
    ^^

--- JobCollectionUnitTest.cs
+++ JobCollectionUnitTest.cs
@@ -8,7 +8,34 @@ namespace UnitTesting
         [TestMethod]
         public void TestJobCollection()
         {
-            
+            //Add Tests
+            [TestMethod]
+            public void NULLAdd()
+            {
+                IJob testJob = null;
+                IJobCollection testJobCollection = new JobCollection(3);
+                Assert.IsFalse(testJobCollection.Add(testJob));
+            }
+
+            [TestMethod]
+            public void FULLAdd()
+            {
+                IJob testJob1 = null;
+                IJob testJob2 = null;
+                IJobCollection testJobCollection = new JobCollection(1);
+                testJobCollection.Add(testJob1);
+                Assert.IsFalse(testJobCollection.Add(testJob2));
+            }
+
+            [TestMethod]
+            public void DUPEAdd()
+            {
+                IJob testJob1 = new Job(543, 92, 27, 7);
+                IJob testJob2 = new Job(543, 92, 27, 7);
+                IJobCollection testJobCollection = new JobCollection(2);
+                testJobCollection.Add(testJob1);
+                Assert.IsFalse(testJobCollection.Add(testJob2));
+            }
         }
     }
 }
